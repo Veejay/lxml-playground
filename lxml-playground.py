13a671bf8bbcc2e21ea11d09df206c7e9648ed74
Bertrand Chardon <bertrand.chardon@gmail.com>
2019-08-04 18:34:58 +0200

    add comments

--- lxml-playground.py
+++ lxml-playground.py
@@ -2,20 +2,27 @@ import lxml
 from lxml import etree
 from io import StringIO
 
-
+# read the contents of the HTML sample
 file = open("./sample.html", "r")
 markup = file.read()
 
+# get a HTML parser instance ready
 parser = etree.HTMLParser()
+
+# parse the markup into an ElementTree
 tree = etree.parse(StringIO(markup), parser)
 print(tree)
 
+# get the root of the tree 
+# (in this case, a <html> tag that the parse method will automatically include for us)
 root = tree.getroot()
 
+# find all <p> tags in the document using XPath
 paragraphs = root.findall('.//p')
 
+# for each paragraph in the document, add a data-attribute
 for paragraph in paragraphs:
-    paragraph.set("data-foo", "bang")
+    paragraph.set("data-foo", "kaboom")
 
-tree = etree.ElementTree(root)
+# write the tree back to a file
 tree.write("result.html")
